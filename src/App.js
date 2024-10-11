e1173f96af974c42a5ec7c7a53d7a7d4dec4be2a
Mitali Karmarkar <mikarmar@microsoft.com>
2024-10-11 12:33:00 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 function App() {
-  const value = 'World! We meet again.';
-  return <div>Hello {value} </div>;
+  const value = 'World';
+  return <div>Hello {value}</div>;
 }
 
 export default App;
