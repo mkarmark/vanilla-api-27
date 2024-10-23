6b95065a49d004a14cc9e419827976c85cd5c3ca
mkarmark <mikarmar@microsoft.com>
2024-10-22 20:43:54 -0700

    Update GetMessage.cs

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -15,8 +15,8 @@ namespace StaticWebAppsEndToEndTesting.GetMessage
         public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
         {
             var response = req.CreateResponse(HttpStatusCode.OK);
-            string dotNetVersion = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription;
-            string message = $"I am running {dotNetVersion}"
+            ar azure_root = Environment.GetEnvironmentVariable("HOME") + @"\site\wwwroot";
+            string message = File.ReadAllText(Path.Join(azure_root, "content.txt"));
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
             response.WriteString(message);
             return response;
